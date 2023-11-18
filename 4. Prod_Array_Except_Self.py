60d1e77d338397f3e45b457f4e16227f3bb3b16f
Prashanth Majety <58392261+MajetyPrashanth@users.noreply.github.com>
2023-11-18 22:31:25 +0530

    Modified 4. Prod_Array_Except_Self.py

--- 4. Prod_Array_Except_Self.py
+++ 4. Prod_Array_Except_Self.py
@@ -1,12 +1,12 @@
 nums = [-1,1,0,-3,3]
-res = [1]*len(nums)
+final = [1]*len(nums)
 prefix = 1
 for i in range(len(nums)):
-    res[i] = prefix
+    final[i] = prefix
     prefix *= nums[i]
         
 postfix = 1
 for i in range(len(nums) -1, -1, -1):
-    res[i] *= postfix
+    final[i] *= postfix
     postfix *= nums[i]
-print(res)
+print(final)
